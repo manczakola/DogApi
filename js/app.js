b13be3a2374e2891350494cc2d9dbb01a32fee9d
Ola Manczak <manczakola@gmail.com>
2020-11-29 13:13:10 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,45 @@
+import '../sass/style.scss'
+
+const imgTag = document.querySelector('img');
+class Doggo {
+    constructor() {
+        this.apiUrl = 'https://dog.ceo/api';
+        this.imgEl = document.querySelector('.featured-dog img');
+        this.backgroundEl = document.querySelector('.featured-dog__background')
+
+        this.init()
+    }
+
+    listBreeds() {
+        return fetch(`${this.apiUrl}/breeds/list/all`)
+            .then(resp => resp.json())
+            .then(data => {
+                return data.message;
+            })
+    };
+
+    getRandomImage() {
+        return fetch(`${this.apiUrl}/breeds/image/random`).then(resp => resp.json()).then(data => data.message);
+    };
+
+
+
+
+    getRandomImageByBreed(breed) {
+        return fetch(`${this.apiUrl}/breed/${breed}/images/random`).then(resp => resp.json()).then(data => data.message)
+    }
+
+    init() {
+        this.getRandomImage().then(src => {
+            this.imgEl.setAttribute('src', src);
+            this.backgroundEl.style.background = `url("${src}")`
+        });
+
+        this.listBreeds().then(breeds => console.log(breeds))
+    }
+
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    new Doggo();
+})
